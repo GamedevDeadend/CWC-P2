16a3df41933675f9b373692b453918bd728a2d86
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-05-22 14:38:51 +0530

    Added Shooting Logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Player;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,6 +8,10 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     public float slidespeed;
     public float xrange = 10.0f;
+
+    public Vector3 offset;
+
+    public GameObject foodProjectile;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        offset = new Vector3 (0,10,0);
+
         if (transform.position.x > xrange)
         {
             transform.position = new Vector3(xrange,transform.position.y, transform.position.z);
@@ -30,6 +38,11 @@ public class PlayerController : MonoBehaviour
         {   
             horizontalInput = Input.GetAxis("Horizontal");
             transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * slidespeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodProjectile, transform.position + offset , foodProjectile.transform.rotation);
         }
     }
 }
